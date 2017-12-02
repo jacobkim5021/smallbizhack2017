57aa5cc4c9b23ab686b660095c4aae4427546dcc
silastsui <silastsuii@gmail.com>
2017-12-02 13:43:37 -0500

    Add users and resume models

---
+++ db/migrate/20171202182113_create_resumes.rb
@@ -0,0 +1,11 @@
+class CreateResumes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :resumes do |t|
+      t.text :content
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :resumes, [:user_id, :created_at]
+  end
+end
