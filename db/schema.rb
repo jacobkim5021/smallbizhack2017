0ede2d5594a8c7b7a805742ea59f0a527e9c9c65
Silas Tsui <silas.tsui@edu.uwaterloo.ca>
2017-12-02 19:51:52 -0500

    Login with cookies (#7)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171202211935) do
+ActiveRecord::Schema.define(version: 20171203001206) do
 
   create_table "resumes", force: :cascade do |t|
     t.text "content"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20171202211935) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
