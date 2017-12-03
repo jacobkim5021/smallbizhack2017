dd91958c79a7a4339c294d32d9847878a647a19f
Silas Tsui <silas.tsui@edu.uwaterloo.ca>
2017-12-02 19:04:38 -0500

    Users (#6)
    * Add sessions and reroute home page
    
    * Add log in functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
       # Log the user in and redirect to the user's show page.
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -14,5 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
