f806817546973dcc3b44592b8d260313dc9c1a8d
silastsui <silastsuii@gmail.com>
2017-12-02 20:05:20 -0500

    Add Jacob's changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
 
   resources :users
   resources :resume
